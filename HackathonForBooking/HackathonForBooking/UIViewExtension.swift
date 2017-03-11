f3c76be6838ee82caafbc6d4ba58c2b307f0ac35
CJ Lin <ejames.lin@gmail.com>
2017-03-12 02:11:45 +0800

    handy utility

---
+++ HackathonForBooking/HackathonForBooking/UIViewExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewExtension.swift
+//  
+//
+//  Created by CJ Lin on 2016/9/20.
+//
+//
+
+import UIKit
+
+extension UIView {
+    class func className() -> String {
+        return String(describing: self)
+    }
+}
