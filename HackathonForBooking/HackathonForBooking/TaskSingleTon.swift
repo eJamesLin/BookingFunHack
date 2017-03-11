85461109769950f5f24898bdb9fd41217099f598
Andy Yang <onionyoung80@gmail.com>
2017-03-11 16:31:19 +0800

    install alamofire, swiftyJSON

---
+++ HackathonForBooking/HackathonForBooking/TaskSingleTon.swift
@@ -0,0 +1,13 @@
+//
+//  TaskSingleTon.swift
+//  HackathonForBooking
+//
+//  Created by Andy Yang on 2017/3/11.
+//  Copyright © 2017年 Andy Yang. All rights reserved.
+//
+
+import UIKit
+
+class TaskSingleTon: NSObject {
+
+}
---
+++ HackathonForBooking/Pods/Target Support Files/Pods-HackathonForBooking/Pods-HackathonForBooking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackathonForBooking : NSObject
+@end
+@implementation PodsDummy_Pods_HackathonForBooking
+@end
