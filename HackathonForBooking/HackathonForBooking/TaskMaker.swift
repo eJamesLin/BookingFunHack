d1b5b21eb9ee9c66a1ea1567ea5fa50d2a1a6401
CJ Lin <ejames.lin@gmail.com>
2017-03-12 02:12:31 +0800

    testcase alter

--- HackathonForBooking/HackathonForBooking/TaskMaker.swift
+++ HackathonForBooking/HackathonForBooking/TaskMaker.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class TaskMaker: NSObject {
     func createTask() -> [TaskObject] {
+        let first = TaskObject(taskID: TaskObject.firstID, title: nil, content: nil)
         let t1 = TaskObject(taskID: 1, title: "吃掉倆倆", content: "至倆倆號購買一份餐點並與之合照，完成這一關。")
+        t1.taskFinished = true
+        t1.shouldBeDisplayed = true
         let t2 = TaskObject(taskID: 2, title: "頂尖 101", content: "至台北101拍攝一張手指頂住101樓頂之合照。")
-        let arr = [t1, t2]
+        t2.shouldBeDisplayed = true
+        let arr = [first, t1, t2]
         return arr
     }
 }
