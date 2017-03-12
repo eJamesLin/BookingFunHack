f7025955439eae393467f92a25d309bd55afea9e
CJ Lin <ejames.lin@gmail.com>
2017-03-12 12:30:35 +0800

    start view controller all challenge lists

---
+++ HackathonForBooking/HackathonForBooking/ChallengeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeCollectionViewCell.swift
+//  HackathonForBooking
+//
+//  Created by CJ Lin on 2017/3/12.
+//  Copyright © 2017年 Andy Yang. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
