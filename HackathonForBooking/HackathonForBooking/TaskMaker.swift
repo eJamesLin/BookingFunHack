20114dd92cc735fd072e0f8b39b0dbe513f2a7dc
CJ Lin <ejames.lin@gmail.com>
2017-03-12 15:30:54 +0800

    task text localization in English

--- HackathonForBooking/HackathonForBooking/TaskMaker.swift
+++ HackathonForBooking/HackathonForBooking/TaskMaker.swift
@@ -12,13 +12,15 @@ class TaskMaker: NSObject {
     func createTask() -> [TaskObject] {
         let first = TaskObject(taskID: TaskObject.firstID, title: nil, content: nil)
 
-        let t1 = TaskObject(taskID: 1, title: "吃掉倆倆", content: "至倆倆號購買一份餐點並與之合照，完成這一關。")
+        let t1 = TaskObject(taskID: 1, title: "Awesome cuisine", content: "Go to the place to buy your breakfast and take a picture.")
         t1.shouldBeDisplayed = true
         t1.isUnlock = true
-        let t2 = TaskObject(taskID: 2, title: "頂尖 101", content: "至台北101拍攝一張手指頂住101樓頂之合照。")
-        let t3 = TaskObject(taskID: 3, title: "任務三", content: "動作。")
-        t1.isUnlock = true
-        let arr = [first, t1, t2, t3]
+        let t2 = TaskObject(taskID: 2, title: "On top of 101", content: "Definitely need a selfie with 101 tower.")
+        let t3 = TaskObject(taskID: 3, title: "Best BEER in the town!!!", content: "Enjoy your relax moment at the bar, share with your friends")
+        t3.isUnlock = true
+        let t4 = TaskObject(taskID: 4, title: "Having some tasty Shit for the sweet trip", content: "Have a coolest selfie at Modern Toilet Restaurant, and smell!!!")
+        t4.isUnlock = true
+        let arr = [first, t1, t2, t3, t4]
         return arr
     }
 }
