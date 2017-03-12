cbf86fb1d235e8f25e640ced1b43f7af02a772f4
CJ Lin <ejames.lin@gmail.com>
2017-03-12 14:50:49 +0800

    answers ui adjust

--- HackathonForBooking/HackathonForBooking/CorrectAnswerViewController.swift
+++ HackathonForBooking/HackathonForBooking/CorrectAnswerViewController.swift
@@ -10,27 +10,11 @@ import UIKit
 
 class CorrectAnswerViewController: UIViewController {
 
+    @IBOutlet weak var actionButton: UIButton!
     @IBOutlet weak var CorrectAnswerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        actionButton.layer.cornerRadius = 7
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
